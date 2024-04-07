e1be539713355fc1ddc438f494c467f24b6b5ab0
Mark Costa <markcosta15@gmail.com>
2024-04-07 19:55:54 +0000

    Made Scatter Plot and Labels, still need linear regressions

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,11 +4,12 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
-
+    df = pd.read_csv('epa-sea-level.csv', header = 0, sep = ',', index_col = 0, parse_dates = True)
+    
     # Create scatter plot
-
-
+    fig, axes = plt.subplots(figsize = (12,6))
+    plt.scatter(x=df.index.year, y = df['CSIRO Adjusted Sea Level'])
+    
     # Create first line of best fit
 
 
@@ -16,6 +17,9 @@ def draw_plot():
 
 
     # Add labels and title
+    axes.set_title('Rise in Sea Level')
+    axes.set_xlabel('Year')
+    axes.set_ylabel('Sea Level (inches)')
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
